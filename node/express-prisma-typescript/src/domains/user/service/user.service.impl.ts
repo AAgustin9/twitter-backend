74780052685354976207b7bbd6851ca2280f52ee
agussoul <aalma@mail.austral.edu.ar>
2025-05-26 09:18:58 -0300

    user/me and /user uses UserViewDto

--- node/express-prisma-typescript/src/domains/user/service/user.service.impl.ts
+++ node/express-prisma-typescript/src/domains/user/service/user.service.impl.ts
@@ -1,22 +1,23 @@
 import { ImageType, getPublicImageUrl, generatePresignedUploadUrl, generateS3Key } from '@utils'
 import { NotFoundException } from '@utils/errors'
 import { OffsetPagination } from 'types'
-import { UserDTO } from '../dto'
+import { UserDTO, UserViewDTO } from '../dto'
 import { UserRepository } from '../repository'
 import { UserService } from './user.service'
 
 export class UserServiceImpl implements UserService {
   constructor (private readonly repository: UserRepository) {}
 
-  async getUser (userId: any): Promise<UserDTO> {
+  async getUser (userId: any): Promise<UserViewDTO> {
     const user = await this.repository.getById(userId)
     if (!user) throw new NotFoundException('user')
-    return user
+    return new UserViewDTO(user)
   }
 
-  async getUserRecommendations (userId: any, options: OffsetPagination): Promise<UserDTO[]> {
+  async getUserRecommendations (userId: any, options: OffsetPagination): Promise<UserViewDTO[]> {
     // TODO: make this return only users followed by users the original user follows
-    return await this.repository.getRecommendedUsersPaginated(options)
+    const users = await this.repository.getRecommendedUsersPaginated(options)
+    return users.map(user => new UserViewDTO(user))
   }
 
   async deleteUser (userId: any): Promise<void> {
@@ -37,12 +38,13 @@ export class UserServiceImpl implements UserService {
     }
   }
   
-  async updateProfileImage(userId: string, imageKey: string): Promise<UserDTO> {
+  async updateProfileImage(userId: string, imageKey: string): Promise<UserViewDTO> {
     // Ensure user exists
     const user = await this.repository.getById(userId)
     if (!user) throw new NotFoundException('user')
     
     // Update the user's profile image
-    return await this.repository.updateProfileImage(userId, imageKey)
+    const updatedUser = await this.repository.updateProfileImage(userId, imageKey)
+    return new UserViewDTO(updatedUser)
   }
 }
--- node/express-prisma-typescript/src/domains/user/service/user.service.ts
+++ node/express-prisma-typescript/src/domains/user/service/user.service.ts
@@ -1,12 +1,12 @@
 import { OffsetPagination } from '@types'
-import { UserDTO } from '../dto'
+import { UserDTO, UserViewDTO } from '../dto'
 
 export interface UserService {
   deleteUser: (userId: any) => Promise<void>
-  getUser: (userId: any) => Promise<UserDTO>
-  getUserRecommendations: (userId: any, options: OffsetPagination) => Promise<UserDTO[]>
+  getUser: (userId: any) => Promise<UserViewDTO>
+  getUserRecommendations: (userId: any, options: OffsetPagination) => Promise<UserViewDTO[]>
   
   // Image operations
   getProfileImageUploadUrl: (userId: string, contentType: string) => Promise<{ uploadUrl: string, profileImageUrl: string }>
-  updateProfileImage: (userId: string, imageKey: string) => Promise<UserDTO>
+  updateProfileImage: (userId: string, imageKey: string) => Promise<UserViewDTO>
 }
